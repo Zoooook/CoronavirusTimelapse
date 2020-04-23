2b80a0b89ab999153e210926488217daf1b0921c
Zoooook <zzzzzzooooook@gmail.com>
2020-04-22 22:16:28 -0600

    remove old version

--- old/getDates.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for filename in os.listdir('originals/'):
-    if filename[-5:] == '.html':
-        text = open('originals/' + filename, 'r', encoding="utf8").read()
-        index = text.find('<iframe src="https://web.archive.org/web/')
-        timestamp = text[index+41:index+55]
-        print(filename[5:12], timestamp[4:6] + '-' + timestamp[6:8] + ' ' + timestamp[8:10] + ':' + timestamp[10:12] + ':' + timestamp[12:14])
--- old/makeGif.py
+++
@@ -1,7 +0,0 @@
-import os
-import imageio
-
-with imageio.get_writer('Coronavirus.gif', mode='I', fps=8) as writer:
-    for filename in os.listdir('images/'):
-        image = imageio.imread('images/' + filename)
-        writer.append_data(image)
